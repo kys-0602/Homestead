2ed68e00dcadf46542603b40e8ea99703d29496b
kys-0602 <i.am.happy.kr@gmail.com>
2026-08-17 11:23:53 +0900

    chore: establish stage 0 project baseline

---
+++ source/Main.cpp
@@ -0,0 +1,9 @@
+#include <Windows.h>
+
+int WINAPI wWinMain(
+    [[maybe_unused]] HINSTANCE instance,
+    [[maybe_unused]] HINSTANCE previousInstance,
+    [[maybe_unused]] PWSTR commandLine,
+    [[maybe_unused]] int showCommand) {
+    return 0;
+}
